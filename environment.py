0992b46f20377c01568e6d04a654c945a8ab4d59
Elias Simojoki <simo.simojoki@gmail.com>
2024-01-20 09:39:25 +0200

    first commit

---
+++ environment.py
@@ -0,0 +1,42 @@
+import itertools
+import random
+
+
+class Game2048:
+
+    def __init__(self, height, width):
+        self.heigh = height
+        self.width = width
+
+        self.grid = [[0 for _ in range(self.width)] for _ in range(self.width)]
+
+
+        def move_left():
+            # the others are handled as transpose of this case
+
+        def add_block(self):
+            # if empty_cells is empty, game lost probably
+            y, x = random.choice(self.empty_cells())
+            num = 2 if random.random() > 0.1 else 4
+            self.grid[y][x] = num
+
+
+        def empty_cells(self):
+            empty_cells = []
+            for y, x in itertools.product(range(self.height), range(self.width)):
+                if self.grid[y][x] == 0:
+                    empty_cells.append((y, x))
+            return empty_cells
+
+
+
+
+
+
+def main():
+    e = Game2048(4, 4)
+    print(e.grid)
+
+
+if __name__ == "__main__":
+    main()
