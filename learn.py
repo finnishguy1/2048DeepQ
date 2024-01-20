66bdd05069c782019eb64cca2960c002042d5c8e
finnishguy1 <nokkapaa@gmail.com>
2024-01-20 14:20:32 +0200

    iterations and episodes

--- learn.py
+++ learn.py
@@ -8,10 +8,12 @@ from environment import Game2048
 model = keras.Sequential()
 
 model.add(keras.Input(shape=(4, 4)))
+model.add(layers.Flatten())
 model.add(layers.Dense(16, activation="relu"))
 model.add(layers.Dense(8, activation="relu"))
 model.add(layers.Dense(4, activation="relu"))
-model.add(layers.Dense(1, activation="linear"))
+# change from 4 to 5 if clear implemented
+model.add(layers.Dense(4, activation="linear"))
 
 model.summary()
 
@@ -23,12 +25,31 @@ def main():
     iterations, limit = (0, 10000)
     epsilon = 0.1
     env = Game2048(4, 4)
-    state = env.state()
+    batchSize = 1
 
-    print(state)
-    Q = model(tf.convert_to_tensor(state))
-    env.move(tf.argmax(Q))
-    print(env.state())
+    sarsa = []
+
+    while iterations < limit:
+        state = env.state()
+        Q = model(tf.convert_to_tensor(state))
+        # implement epsilon greedy here
+        action = tf.argmax(Q)
+
+        env.move(action)
+        reward = env.score()
+        nextState = env.state()
+        nextAction = tf.argmax(model(tf.convert_to_tensor(nextState)))
+
+        sarsa.append([state, action, reward, nextState, nextAction])
+
+        # for later to change batch size, train model then empty the sarsa
+        if iterations % batchSize == 0:
+            for list, i in enumerate(sarsa):
+                pass
+
+            sarsa = []
+
+        iterations += 1
 
 
 if __name__ == "__main__":
